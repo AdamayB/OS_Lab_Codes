25c987ad53646165ba1257827dcd159a82fe95ca
Adamay Bhardwaj <92214282+AdamayB@users.noreply.github.com>
2023-11-17 22:31:59 +0530

    Update fcfs.py

--- fcfs.py
+++ fcfs.py
@@ -1,18 +1,27 @@
-def findWaitingTime(processes,n,bt,wt):
+def fcfs(process,n):
+    wt=[0]*n
+
+    index = 0
+    for i in range(n - 1):
+        index = i
+        for j in range(i + 1, n):
+            if process[j][0] < process[index][0]:
+                index = j
+
+        process[i], process[index] = process[index], process[i]
+
+    bTime = [process[i][1] for i in range(n)]
     wt[0]=0
     ct=[0]*n
-    ct[0]=bt[0]
+    ct[0]=bTime[0]
     for i in range(1,n):
-        wt[i]=bt[i-1]+wt[i-1]
-        ct[i]=ct[i-1]+bt[i]
+        wt[i]=wt[i-1]+bTime[i-1]
+        ct[i]=ct[i-1]+bTime[i]
 
-    return wt,ct
+    return ct,wt
 
-
-processes = [1, 2, 3]
-n = len(processes)
-
-# Burst time of all processes
-burst_time = [10, 5, 8]
-wt=[0]*n
-print(findWaitingTime(processes, n, burst_time,wt))
+process=[[4, 5],
+         [3, 6],
+         [1, 7],
+         [5, 5]]
+print(fcfs(process,4))
